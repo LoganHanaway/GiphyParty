ecf72c7503d24908661ddcd03ac4a159d17742ea
Logan Hanaway <lthanaway14@gmail.com>
2024-03-14 21:22:59 -0500

    query stuck on undefinied

--- app.js
+++ app.js
@@ -2,18 +2,26 @@
 const token = "1tRxIpVB1wcaQmqPHwrOlSlxFlL97g14";
 const $search = $("#search");
 const $remove = $("#remove");
+const $resultsContainer = $("#resultsContainer");
 
 // function to get the search term
-const q = function getSearchTerm() {
-  return $("#userInput").val();
-};
+function getSearchTerm() {
+  const query = $("#userInput").val();
+  return query;
+}
 
 // function to submit the search
-async function submitSearch(q) {
+async function submitSearch() {
   const res = await axios.get(
-    `https://api.giphy.com/v1/gifs/search?api_key=1tRxIpVB1wcaQmqPHwrOlSlxFlL97g14&q=${q}&limit=1&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+    `https://api.giphy.com/v1/gifs/search?api_key=1tRxIpVB1wcaQmqPHwrOlSlxFlL97g14&q=${query}&limit=1&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
   );
   console.log(res);
+  const url = res.data.data[0].embed_url;
+  const card = document.createElement("div");
+  $(card).attr("class", "card");
+  const gif = document.createElement("img");
+  $(gif).attr("src", url);
+  $(gif).appendTo(card).appendTo($resultsContainer);
 }
 
 // function to apend search result to container
